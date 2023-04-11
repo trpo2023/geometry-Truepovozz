cc53827096e64714af753ccc3a7763fe4961962e
Truep <pvorman54@gmail.com>
2023-04-11 22:03:05 +0700

    trying to fix build errors

--- src/libgeometry/shape_info.h
+++ src/libgeometry/shape_info.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "geometry.h"
-#include <stddef.h>
 #include <math.h>
+#include <stddef.h>
 
 #define SHAPE_INFO_SIZE 20
 
-void get_shape_info(char*, size_t, char*); // string with shape, string`s length and string for info
 float get_perimeter(circle*);
 float get_area(circle*);
