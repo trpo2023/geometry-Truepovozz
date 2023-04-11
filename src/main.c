56ed9fe49fa224b26e34c01598b2faff9a4e843c
Truep <pvorman54@gmail.com>
2023-04-11 21:14:48 +0700

    clang-format

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <ctype.h>
+#include <libgeometry/geometry.h>
+#include <libgeometry/shape_info.h>
+#include <libgeometry/syntax.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <libgeometry/geometry.h>
-#include <libgeometry/syntax.h>
-#include <libgeometry/shape_info.h>
 
 #define SHAPE_INFO_SIZE 20
 #define QUONTITY_OF_SHAPES 2
@@ -19,13 +19,16 @@ int main(int argc, char* argv[])
     char* shape_str = NULL;
     size_t size_of_shape_str = 0;
     circle* shapes[QUONTITY_OF_SHAPES];
-    for(int i = 0; i < QUONTITY_OF_SHAPES; i++)
-    {
+    for (int i = 0; i < QUONTITY_OF_SHAPES; i++) {
         shapes[i] = malloc(sizeof(circle));
         getline(&shape_str, &size_of_shape_str, file);
 
         if (syntax_check(shape_str, size_of_shape_str)) {
-            sscanf(shape_str, "circle(%f %f, %f)", &shapes[i]->center_x, &shapes[i]->center_y, &shapes[i]->radius);
+            sscanf(shape_str,
+                   "circle(%f %f, %f)",
+                   &shapes[i]->center_x,
+                   &shapes[i]->center_y,
+                   &shapes[i]->radius);
         } else {
             printf("Please correct your file and try again:3\n");
             return 0;
@@ -33,9 +36,11 @@ int main(int argc, char* argv[])
         free(shape_str);
     }
 
-    for(int i = 0; i < QUONTITY_OF_SHAPES; i++)
-    {
-        printf("circle(%f %f, %f)\n", shapes[i]->center_x, shapes[i]->center_y, shapes[i]->radius);
+    for (int i = 0; i < QUONTITY_OF_SHAPES; i++) {
+        printf("circle(%f %f, %f)\n",
+               shapes[i]->center_x,
+               shapes[i]->center_y,
+               shapes[i]->radius);
         printf("perimeter = %f\n", get_perimeter(shapes[i]));
         printf("area = %f\n", get_area(shapes[i]));
     }
